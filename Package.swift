406f17fed2d9291f8b9342a1a1d52cc34c6baf67
Mohssen Fathi <mmohssenfathi@gmail.com>
2019-10-11 10:14:53 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Notification Banner",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "Notification Banner", targets: ["NotificationBannerSwift"]),
+    ],
+    dependencies: [
+        .package(name: "SnapKit", from: "5.0.0"),
+        .package(url: "https://github.com/cbpowell/MarqueeLabel.git", .branch("xcode11"))
+        
+    ],
+    targets: [
+        .target(name: "NotificationBannerSwift", dependencies: ["NotificationBannerSwift"])
+    ]
+)
+
